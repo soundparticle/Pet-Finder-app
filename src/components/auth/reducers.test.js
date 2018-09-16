05d6af9f13ebb1761719609760db6d61c8b0351c
Easton Gorishek <ieaston26@gmail.com>
2018-09-16 16:16:37 -0700

    auth reducer tests passing

--- src/components/auth/reducers.test.js
+++ src/components/auth/reducers.test.js
@@ -1,8 +1,10 @@
 import {
   user,
-  // getUser,
-  // USER_AUTH,
-  // LOGOUT
+  getUser,
+  USER_AUTH,
+  LOGOUT,
+  checkedAuth,
+  CHECKED_AUTH
 } from './reducers';
 
 describe ('user reducer', () => {
@@ -12,5 +14,29 @@ describe ('user reducer', () => {
     expect(state).toBe(null);
   });
 
+  it('checks auth', () => {
+    const state = checkedAuth(false, { type: CHECKED_AUTH });
+    expect(state).toBe(true);
+  });
+
+  it('loads a user', () => {
+    const data = {
+      name: 'user',
+      email: 'test@test.com'
+    };
+
+    const state = user(null, { type: USER_AUTH, payload: data });
+    expect(state).toEqual(data);
+  });
+
+  it('clears user on logout', () => {
+    const state = user({}, { type: LOGOUT });
+    expect(state).toBe(null);
+  });
+
+  it('gets user from state', () => {
+    const user = { name: 'user' };
+    expect(getUser({ user })).toBe(user);
+  });
 
 });
