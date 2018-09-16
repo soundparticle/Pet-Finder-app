e311ca82330265b66da45554b23e3e981a4cd723
Antreo Pukay <acrosswalker@yahoo.com>
2018-09-16 13:13:37 -0700

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React, { PureComponent } from 'react';
+
+
+class App extends PureComponent {
+
+  render() {
+    
+    return (
+      <h1>Hello Team Cookie!</h1>
+    );
+  }
+}
+export default App;
