b65765c78ef832cfaee3c5461d7ceb3c942945ab
Robyn Navarro <rnavarro182@gmail.com>
2018-09-17 18:38:07 -0700

    dashboard started

--- src/components/pets/PetDashboard.js
+++ src/components/pets/PetDashboard.js
@@ -1,14 +1,40 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { getInterested, getFavorites } from './reducers';
+import { loadInterested, loadFavorites } from './actions';
+// import { data } from '../../mock-data/mock-data';
 
 class PetDashboard extends Component {
 
+  static propTypes = {
+    loadInterested: PropTypes.func.isRequired,
+    loadFavorites: PropTypes.func.isRequired,
+    pets: PropTypes.array
+  };
+
+  componentDidMount() {
+    this.props.loadInterested();
+    this.props.loadFavorites();
+  }
   render() { 
+    const { pets } = this.props;
     return ( 
       <div>
         <h2>Pet Dashboard</h2>
+        <h3>Interested in:</h3>
+        <ul>
+          Pet list here
+        </ul>
       </div>
     );
   }
 }
  
-export default PetDashboard;
+export default connect(
+  state => ({ 
+    pets: getInterested(state), 
+    favPets: getFavorites(state)
+  }),
+  { loadInterested }
+)(PetDashboard);
--- src/store/store.js
+++ src/store/store.js
@@ -2,13 +2,16 @@ import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import promiseMiddleware from './promise-middleware';
 import { error, loading } from '../components/app/reducers';
+import { favorites, interested } from '../components/pets/reducers';
 import { user, checkedAuth } from '../components/auth/reducers';
 
 const rootReducer = combineReducers({
   error,
   loading,
   user,
-  checkedAuth
+  checkedAuth,
+  favorites,
+  interested
 });
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
