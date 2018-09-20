be2d9da9e717a899f0c7899453442e0c121cf29b
Easton Gorishek <ieaston26@gmail.com>
2018-09-20 10:30:48 -0700

    fixed error in GetStarted component

--- src/components/app/GetStarted.js
+++ src/components/app/GetStarted.js
@@ -1,8 +1,12 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 import styles from './GetStarted.css';
 import Modal from 'react-responsive-modal';
 import PetSeeker from '../forms/PetSeeker';
 import PostForm from '../forms/PostForm';
+import { addSeeker } from '../pets/actions';
+import { addPet } from '../pets/actionsPets';
 
 
 class GetStarted extends Component {
@@ -11,6 +15,11 @@ class GetStarted extends Component {
     open: false,
     open2: false
   };
+
+  static propTypes = {
+    addSeeker: PropTypes.func.isRequired,
+    addPet: PropTypes.func.isRequired
+  }
  
   onOpenModal = () => {
     this.setState({ open: true });
@@ -28,7 +37,9 @@ class GetStarted extends Component {
     this.setState({ open2: false });
   };
   render() { 
+
     const { open, open2 } = this.state;
+    const { addSeeker, addPet } = this.props;
     return (
       <div className={styles.getStarted}>
         <header>
@@ -38,7 +49,7 @@ class GetStarted extends Component {
           <p> If you&apos;re looking for a new cat or dog, just fill out the form requesting details about your lifestyle and what you&apos;re looking for, and we&apos;ll find the most ideal matches for you. Its that easy!</p>
           <button onClick={this.onOpenModal}>Find a pet</button>
           <Modal classNames={styles} open={open} onClose={this.onCloseModal} center={false} animationDuration={100} showCloseIcon={false}>
-            <PetSeeker/>
+            <PetSeeker onComplete={addSeeker}/>
           </Modal>
         </section>
 
@@ -46,7 +57,7 @@ class GetStarted extends Component {
           <p>Looking for a home for your pet? Post you pet info here!</p>
           <button onClick={this.onOpenModel2}>Post a pet</button>
           <Modal classNames={styles} open={open2} onClose={this.onCloseModel2} center={false} animationDuration={100} showCloseIcon={false}>
-            <PostForm/>
+            <PostForm onComplete={addPet}/>
           </Modal>
         </section>
       </div>
@@ -54,4 +65,8 @@ class GetStarted extends Component {
   }
 }
  
-export default GetStarted;
+export default connect(
+  null,
+  { addSeeker, addPet }
+
+)(GetStarted);
