4643c665ab7b42c60ed376ecffa2c879032a5eeb
Easton Gorishek <ieaston26@gmail.com>
2018-09-20 15:50:31 -0700

    made PetTile component

--- src/components/imageSlider/Slider.js
+++ src/components/imageSlider/Slider.js
@@ -5,8 +5,7 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { getSeeker } from '../pets/reducers';
 import { getPets } from '../pets/reducersPets';
-import FavoriteButton from '../controls/FavoriteButton';
-import WantButton from '../controls/WantButton';
+import PetTile from './PetTile';
 import styles from './Slider.css';
 
 class Slider extends Component {
@@ -14,11 +13,11 @@ class Slider extends Component {
   static propTypes = {
     seeker: PropTypes.object,
     pets: PropTypes.array,
-    addFavorite: PropTypes.func.isRequired
+    addFavorite: PropTypes.func.isRequired,
+    addWant: PropTypes.func.isRequired
   }
 
   state = {
-    images: [{ image: 1, id: 1 }, { image: 2, id: 2 }, { image: 3, id: 3 }, { image: 4, id: 4 }],
     slideCount: 0
   };
 
@@ -41,45 +40,22 @@ class Slider extends Component {
     this.setState({ slideCount: slideCount - 1 });
   }
 
-  // handleFavorite = pet => {
-  //   this.props.addFavorite(pet);
-  // }
-
-
   render() { 
     const { slideCount } = this.state;
-    const { addFavorite, pets } = this.props;
+    const { addFavorite, addWant, pets } = this.props;
 
     return (
       <div className={styles.slider}>
         {pets.map((pet) => {
           if(pets.indexOf(pet) === slideCount) {
-
-            return (
-              <div className='petTile' key={pet._id}>
-                <header>
-                  <h3>{pet.name}</h3>
-                </header>
-                <img src={pet.images}></img>
-
-                <section className='buttons'>
-                  <FavoriteButton 
-                    onComplete={addFavorite}
-                    pet={pet}
-                  />
-                  <WantButton 
-                    onComplete={addWant}
-                    pet={pet}
-                  />
-                </section>
-
-                <section>
-                  <p>Description a loa;sdhgl;ajshdg ;lashdglha;s dg l;ajhsdgjhas;djg ajshdg ajshdg;l a;lsdgh asd ;alksdg lahsd;lgha;sdhg;ahsd;gh a;sd g</p>
-                </section>
-              </div>
-            );
+            return <PetTile
+              key={pet._id} 
+              pet={pet}
+              addFavorite={addFavorite}
+              addWant={addWant} 
+            />;
           }
-          return '';
+          else return null;
         })}
 
         <section>
