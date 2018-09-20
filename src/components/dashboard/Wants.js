092f3ed95d171400d57c4204d2068684f0d1c843
Easton Gorishek <ieaston26@gmail.com>
2018-09-19 18:53:48 -0700

    able to add favorite to seeker document

---
+++ src/components/dashboard/Wants.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Wants extends Component {
+  state = {  }
+  render() { 
+    return ( 
+      <h3>Wants Component</h3>
+    );
+  }
+}
+ 
+export default Wants;
