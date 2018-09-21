babbb282fef2e704b062c1b1d975a406666a7982
Easton Gorishek <ieaston26@gmail.com>
2018-09-21 12:56:48 -0700

    final touches

--- src/components/imageSlider/PetTile.js
+++ src/components/imageSlider/PetTile.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import FavoriteButton from '../controls/FavoriteButton';
 import WantButton from '../controls/WantButton';
 import styles from './PetTile.css';
 import PetDetailView from '../pets/PetDetailView';
@@ -9,26 +8,21 @@ class PetTile extends Component {
 
   static propTypes = {
     pet: PropTypes.object,
-    addFavorite: PropTypes.func.isRequired,
     addWant: PropTypes.func.isRequired
   }
 
   render() { 
-    const { pet, addWant, addFavorite } = this.props;
+    const { pet, addWant } = this.props;
 
     return (
       <div className={styles.petTile} key={pet._id}>
         <hgroup>
-          <h3>{pet.name}</h3>
-          <h4>{pet.catBreed ? pet.catBreed : pet.dogBreed}</h4>
+          <h3>Name: {pet.name}</h3>
+          <h4>Breed: {pet.catBreed ? pet.catBreed : pet.dogBreed}</h4>
         </hgroup>
         <img src={pet.images}></img>
 
         <section className="buttons">
-          <FavoriteButton 
-            onComplete={addFavorite}
-            pet={pet}
-          />
           <WantButton 
             onComplete={addWant}
             pet={pet}
