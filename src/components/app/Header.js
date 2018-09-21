2ad6bfd2c37345d32421759b2b0992203195d4db
Robyn Navarro <rnavarro182@gmail.com>
2018-09-21 10:14:46 -0700

    new logo

--- src/components/app/Header.js
+++ src/components/app/Header.js
@@ -6,7 +6,7 @@ import { logout } from '../auth/actions';
 import { Link } from 'react-router-dom'; //Route ?
 import Error from './Error';
 import styles from './Header.css';
-import logo from '../../assets/images/logo-2.svg';
+import logo from '../../assets/images/logo-3.svg';
 
 class Header extends Component {
 
