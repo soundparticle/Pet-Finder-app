f5e349a33e0cdb1542d81d88b716f6c1a12e7f1b
Rehmsy <sarahrehmer@gmail.com>
2018-09-21 12:14:38 -0700

    styles

--- src/components/app/Home.js
+++ src/components/app/Home.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 // import Dropzone from 'react-dropzone';
 import axios from 'axios';
 import styles from './Home.css';
+import dogAndCat from '../../assets/images/dogAndCat.png';
 // import yourImage from '../.././assets/images/white-canine.jpg';
 // import background from '../.././assets/images/white-fur-texture.jpeg';
 
@@ -56,10 +57,13 @@ class Home extends Component {
     return (
       <div className={styles.home}>
         {/* <h1>Home/About Page</h1> */}
-        <h2>Welcome to Home Swap</h2>
+        <h2>Every pet deserves a home.</h2>
         <div className="container">
           <div className="summary">
             <p>Looking for the perfect pet for your home or need to find a home for a pet? We can help! Follow the links above and you&apos;re on your way!</p>
+          </div>
+          <div className="image">
+            <img src={dogAndCat}  />
           </div>
           
           {/* <div className={styles.container}>
